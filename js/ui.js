025d196cc86a9a5005c8883b1686e33cd1a1c9ed
Misael Portillo <misaelrp28@gmail.com>
2024-10-04 15:38:00 -0500

    second

--- js/ui.js
+++ js/ui.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     //Sidenav Init
     const menus = document.querySelector(".sidenav");
     M.Sidenav.init(menus, {edge: "right"});
-    //Add task
-    const forms = document.querySelector(".side-form");
-    M.Sidenav.init(forms, {edge: "left"});
+
+    //Modal
+    var elems = document.querySelectorAll('.modal');
+     M.Modal.init(elems, options);
 });
 
 
@@ -14,11 +15,6 @@ if("serviceWorker" in navigator){
     .then(req => console.log("Service Worker Registered", req))
     .catch(err => console.log("Service Worker registration failed.", err));
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-     M.Modal.init(elems, options);
-  });
 
   $(document).ready(function(){
     $('.modal').modal();
--- serviceworker.js
+++ serviceworker.js
@@ -1,85 +1,85 @@
-const CACHE_NAME = "task-manager-v5";
+// const CACHE_NAME = "task-manager-v5";
 
-const ASSETS_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/pages/about.html",
-    "/pages/contact.html",
-    "/css/materialize.min.css",
-    "/js/materialize.min.js",
-    "/js/ui.js",
-    "/img/task.png"
-];
+// const ASSETS_TO_CACHE = [
+//     "/",
+//     "/index.html",
+//     "/pages/about.html",
+//     "/pages/contact.html",
+//     "/css/materialize.min.css",
+//     "/js/materialize.min.js",
+//     "/js/ui.js",
+//     "/img/task.png"
+// ];
 
-//Install
-self.addEventListener("install", (event) => {
-    console.log("Service worker: Installing...");
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("Service worker: Caching files");
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
-});
+// //Install
+// self.addEventListener("install", (event) => {
+//     console.log("Service worker: Installing...");
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then((cache) => {
+//             console.log("Service worker: Caching files");
+//             return cache.addAll(ASSETS_TO_CACHE);
+//         })
+//     );
+// });
 
-//Activate
-self.addEventListener('activate', (event) =>{
-    console.log("Service Worker: Activating...");
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if(cache !== CACHE_NAME){
-                        console.log("Service Worker: Deleting old Cache");
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
-
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        (async function(){
-            const cachedResponse = await caches.match(event.request);
-
-            if(cachedResponse){
-                return cachedResponse;
-            }
-
-            try{
-                const networkResponse = await fetch(event.request);
-                const cache = await caches.open(CACHE_NAME);
-                cache.put(event.request, networkResponse.clone()); //Update cache with the fetched response
-                return networkResponse;
-            } catch(error){
-                console.error("Fetch failed, returning offline page: ", error);
-                //Optionallyu return an offline page here if available in the cache
-            }
-        })()
-    );
-});
+// //Activate
+// self.addEventListener('activate', (event) =>{
+//     console.log("Service Worker: Activating...");
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => {
+//             return Promise.all(
+//                 cacheNames.map((cache) => {
+//                     if(cache !== CACHE_NAME){
+//                         console.log("Service Worker: Deleting old Cache");
+//                         return caches.delete(cache);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+// });
 
 
+// self.addEventListener("fetch", (event) => {
+//     event.respondWith(
+//         (async function(){
+//             const cachedResponse = await caches.match(event.request);
+
+//             if(cachedResponse){
+//                 return cachedResponse;
+//             }
+
+//             try{
+//                 const networkResponse = await fetch(event.request);
+//                 const cache = await caches.open(CACHE_NAME);
+//                 cache.put(event.request, networkResponse.clone()); //Update cache with the fetched response
+//                 return networkResponse;
+//             } catch(error){
+//                 console.error("Fetch failed, returning offline page: ", error);
+//                 //Optionallyu return an offline page here if available in the cache
+//             }
+//         })()
+//     );
+// });
 
 
-//Fetch
-self.addEventListener("fetch", (event) => {
-    console.log("Service Worker: Fetching...", event.request.url);
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            if(cachedResponse){
-                return cachedResponse;
-            }
 
-            return fetch(event.request).then((networkResponse) => {
-                return caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, networkResponse.clone());
-                    return networkResponse;
-                })
-            })
-        })
-    );
-});
+
+// //Fetch
+// self.addEventListener("fetch", (event) => {
+//     console.log("Service Worker: Fetching...", event.request.url);
+//     event.respondWith(
+//         caches.match(event.request).then((cachedResponse) => {
+//             if(cachedResponse){
+//                 return cachedResponse;
+//             }
+
+//             return fetch(event.request).then((networkResponse) => {
+//                 return caches.open(CACHE_NAME).then((cache) => {
+//                     cache.put(event.request, networkResponse.clone());
+//                     return networkResponse;
+//                 })
+//             })
+//         })
+//     );
+// });

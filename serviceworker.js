f7d03c6b818d92023b3a0ab4a6d70a2724bf434c
Misael Portillo <misaelrp28@gmail.com>
2024-12-10 17:03:37 -0600

    Working on offline delete

--- serviceworker.js
+++ serviceworker.js
@@ -25,7 +25,7 @@ messaging.onBackgroundMessage(function (payload){
   self.ServiceWorkerRegistration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = "task-manager-v6";
+const CACHE_NAME = "workout-buddy-v1";
 
 const ASSETS_TO_CACHE = [
     "/",
